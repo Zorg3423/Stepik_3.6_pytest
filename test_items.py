2c744d2bedc9919bf3aa06214834fceedda5579e
Zorg3423 <deed.sada@bk.ru>
2025-12-31 08:06:06 +0300

    Add files via upload

--- test_items.py
+++ test_items.py
@@ -5,10 +5,10 @@ def test_guest_should_see_add_to_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
 
-    time.sleep(10)
+    time.sleep(30)
 
     add_to_basket_button = browser.find_element(
         "css selector", "button.btn-add-to-basket"
     )
 
-    assert len(add_to_basket_button) > 0, "Add to basket button is not found"
+    assert add_to_basket_button is not None, "Add to basket button is not found"
